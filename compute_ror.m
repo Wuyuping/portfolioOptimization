d9d0014307e1482af346d821180a04c03b83ea61
Thanh Doan <tcdoan@gmail.com>
2011-05-07 19:59:33 -0500

    update compute_ror.m to compute daily rate of return and combine all 14 rates into a dataset

--- compute_ror.m
+++ compute_ror.m
@@ -26,6 +26,28 @@ function compute_ror()
     IYR_dat = computeRate(IYR_dat);
     
     save('ETF14_ROR_Feb05_Dec10.mat');
+
+    % Combine rate into one datasdet
+    dateVec = SPY_dat.Date;
+    [n, ~] = size(SPY_dat);
+    dayRateMat = zeros(n,14);    
+    dayRateMat(:,1) = SPY_dat.Rate;
+    dayRateMat(:,2) = IJH_dat.Rate;
+    dayRateMat(:,3) = IJR_dat.Rate;
+    dayRateMat(:,4) = IYY_dat.Rate;
+    
+    dayRateMat(:,5) = XLE_dat.Rate;
+    dayRateMat(:,6) = EWZ_dat.Rate;
+    dayRateMat(:,7) = EWJ_dat.Rate;
+    dayRateMat(:,8) = EWH_dat.Rate;
+    
+    dayRateMat(:,9) = EEM_dat.Rate;
+    dayRateMat(:,10) = EZU_dat.Rate;
+    dayRateMat(:,11) = EFA_dat.Rate;
+    dayRateMat(:,12) = AGG_dat.Rate;
+    dayRateMat(:,13) = IAU_dat.Rate;
+    dayRateMat(:,14) = IYR_dat.Rate;
+    combineROR(dateVec, dayRateMat);
 end
 
 function newdataset = computeRate(data) 
@@ -35,7 +57,11 @@ function newdataset = computeRate(data)
     newdataset = data;
 end
 
-
+function combineROR(dateVec,dayRateMat)
+    outfile = 'dayROR_ETF14';
+    dayROR_ETF14 = dataset({dateVec,'Date'}, {dayRateMat, 'SPY', 'IJH','IJR','IYY','XLE', 'EWZ','EWJ','EWH','EEM','EZU','EFA','AGG','IAU','IYR'});
+    save(outfile,'-v7.3','dayROR_ETF14');
+end
 
 
 
