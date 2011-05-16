915d31f2d741e3c6d7c4a1d953a5d086ec94527c
Thanh Doan <tcdoan@gmail.com>
2011-05-16 00:50:32 -0500

    rewrite download script to has historical data from  old to recent date order

--- download_etf.m
+++ download_etf.m
@@ -14,49 +14,63 @@ function download_etf()
     
     % SPY IJH IJR	IYY
     SPY_mat = fetching('SPY');
-    SPY_dat = convert_toDS(SPY_mat);
+    SPY = convert_toDS(SPY_mat);
+    clear SPY_mat;
     
     IJH_mat = fetching('IJH');
-    IJH_dat = convert_toDS(IJH_mat);
+    IJH = convert_toDS(IJH_mat);
+    clear IJH_mat;
     
     IJR_mat = fetching('IJR');
     IJR_dat = convert_toDS(IJR_mat);
+    clear IJR_mat;
     
     IYY_mat = fetching('IYY');
-    IYY_dat = convert_toDS(IYY_mat);
+    IYY = convert_toDS(IYY_mat);
+    clear IYY_mat;
     
     % XLE	EWZ	EWJ	EWH	
     XLE_mat = fetching('XLE');
-    XLE_dat = convert_toDS(XLE_mat);
+    XLE = convert_toDS(XLE_mat);
+    clear XLE_mat; 
     
     EWZ_mat = fetching('EWZ');
-    EWZ_dat = convert_toDS(EWZ_mat);
+    EWZ  = convert_toDS(EWZ_mat);
+    clear EWZ_mat;
     
     EWJ_mat = fetching('EWJ');
-    EWJ_dat = convert_toDS(EWJ_mat);
+    EWJ = convert_toDS(EWJ_mat);
+    clear EWJ_mat;
     
     EWH_mat = fetching('EWH');
-    EWH_dat = convert_toDS(EWH_mat);
-
+    EWH = convert_toDS(EWH_mat);
+    clear EWH_mat;
+    
     % EEM	EZU	EFA	AGG	
     EEM_mat = fetching('EEM');
-    EEM_dat = convert_toDS(EEM_mat);
+    EEM = convert_toDS(EEM_mat);
+    clear EEM_mat;
     
     EZU_mat = fetching('EZU');
-    EZU_dat = convert_toDS(EZU_mat);
+    EZU = convert_toDS(EZU_mat);
+    clear EZU_mat;
     
     EFA_mat = fetching('EFA');
-    EFA_dat = convert_toDS(EFA_mat);
+    EFA = convert_toDS(EFA_mat);
+    clear EFA_mat;
     
     AGG_mat = fetching('AGG');
-    AGG_dat = convert_toDS(AGG_mat);
+    AGG = convert_toDS(AGG_mat);
+    clear AGG_mat;
 
     % IAU IYR
     IAU_mat = fetching('IAU');
-    IAU_dat = convert_toDS(IAU_mat);
+    IAU = convert_toDS(IAU_mat);
+    clear IAU_mat;
     
     IYR_mat = fetching('IYR');
-    IYR_dat = convert_toDS(IYR_mat);
+    IYR = convert_toDS(IYR_mat);
+    clear IYR_mat;    
     
     save('ETF14_Feb05_Dec10.mat');
 end
@@ -67,5 +81,8 @@ function datamat = fetching(etf_symbol)
 end
 
 function myds = convert_toDS(data_mat)
-    myds = dataset({cellstr(datestr(data_mat(:,1),'mm/dd/yyyy')),'Date'},{data_mat(:,2:7), 'Close', 'High', 'Low', 'Open', 'Volume', 'Adj_Close'});
+    nrows = size(data_mat, 1);
+    reverse_idx = nrows : -1 : 1;
+    new_mat = data_mat(reverse_idx, :);
+    myds = dataset({cellstr(datestr(new_mat(:,1),'mm/dd/yyyy')),'Date'},{new_mat(:,2:7), 'Close', 'High', 'Low', 'Open', 'Volume', 'Adj_Close'});
 end
