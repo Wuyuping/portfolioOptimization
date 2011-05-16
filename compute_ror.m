5243d07ced935391df0493e9e823a625779373cb
Thanh Doan <tcdoan@gmail.com>
2011-05-16 01:03:50 -0500

    rewrite compute_ror to put all data in the struct ETF14

--- compute_ror.m
+++ compute_ror.m
@@ -4,64 +4,72 @@ function compute_ror()
     load ETF14_Feb05_Dec10.mat;
     
     % SPY IJH	IJR	IYY	
-    SPY_dat = computeRate(SPY_dat);
-    IJH_dat = computeRate(IJH_dat);
-    IJR_dat = computeRate(IJR_dat);
-    IYY_dat = computeRate(IYY_dat);
+    SPY = computeRate(SPY);
+    IJH = computeRate(IJH);
+    IJR = computeRate(IJR);
+    IYY = computeRate(IYY);
   
     % XLE	EWZ	EWJ	EWH	    
-    XLE_dat = computeRate(XLE_dat);
-    EWZ_dat = computeRate(EWZ_dat);
-    EWJ_dat = computeRate(EWJ_dat);
-    EWH_dat = computeRate(EWH_dat);
+    XLE = computeRate(XLE);
+    EWZ = computeRate(EWZ);
+    EWJ = computeRate(EWJ);
+    EWH = computeRate(EWH);
 
     % EEM	EZU	EFA	AGG	    
-    EEM_dat = computeRate(EEM_dat);
-    EZU_dat = computeRate(EZU_dat);
-    EFA_dat = computeRate(EFA_dat);
-    AGG_dat = computeRate(AGG_dat);
+    EEM = computeRate(EEM);
+    EZU = computeRate(EZU);
+    EFA = computeRate(EFA);
+    AGG = computeRate(AGG);
 
     % IAU	IYR    
-    IAU_dat = computeRate(IAU_dat);
-    IYR_dat = computeRate(IYR_dat);
+    IAU = computeRate(IAU);
+    IYR = computeRate(IYR);
     
-    save('ETF14_ROR_Feb05_Dec10.mat');
-
     % Combine rate into one datasdet
-    dateVec = SPY_dat.Date;
-    [n, ~] = size(SPY_dat);
+    dateVec = SPY.Date;
+    [n, ~] = size(SPY);
     dayRateMat = zeros(n,14);    
-    dayRateMat(:,1) = SPY_dat.Rate;
-    dayRateMat(:,2) = IJH_dat.Rate;
-    dayRateMat(:,3) = IJR_dat.Rate;
-    dayRateMat(:,4) = IYY_dat.Rate;
+    dayRateMat(:,1) = SPY.Rate;
+    dayRateMat(:,2) = IJH.Rate;
+    dayRateMat(:,3) = IJR.Rate;
+    dayRateMat(:,4) = IYY.Rate;
     
-    dayRateMat(:,5) = XLE_dat.Rate;
-    dayRateMat(:,6) = EWZ_dat.Rate;
-    dayRateMat(:,7) = EWJ_dat.Rate;
-    dayRateMat(:,8) = EWH_dat.Rate;
+    dayRateMat(:,5) = XLE.Rate;
+    dayRateMat(:,6) = EWZ.Rate;
+    dayRateMat(:,7) = EWJ.Rate;
+    dayRateMat(:,8) = EWH.Rate;
     
-    dayRateMat(:,9) = EEM_dat.Rate;
-    dayRateMat(:,10) = EZU_dat.Rate;
-    dayRateMat(:,11) = EFA_dat.Rate;
-    dayRateMat(:,12) = AGG_dat.Rate;
-    dayRateMat(:,13) = IAU_dat.Rate;
-    dayRateMat(:,14) = IYR_dat.Rate;
-    combineROR(dateVec, dayRateMat);
+    dayRateMat(:,9) = EEM.Rate;
+    dayRateMat(:,10) = EZU.Rate;
+    dayRateMat(:,11) = EFA.Rate;
+    dayRateMat(:,12) = AGG.Rate;
+    dayRateMat(:,13) = IAU.Rate;
+    dayRateMat(:,14) = IYR.Rate;
+    dayRateSet = dataset({dateVec,'Date'}, {dayRateMat, 'SPY', 'IJH','IJR','IYY','XLE', 'EWZ','EWJ','EWH','EEM','EZU','EFA','AGG','IAU','IYR'});
+       
+    ETF14.SPY =   SPY;
+    ETF14.IJH =   IJH;
+    ETF14.IJR =   IJR;
+    ETF14.IYY =   IYY;
+    ETF14.XLE =   XLE;
+    ETF14.EWZ =   EWZ;
+    ETF14.EWJ =   EWJ;
+    ETF14.EWH =   EWH;
+    ETF14.EEM =   EEM;
+    ETF14.EZU =   EZU;
+    ETF14.EFA =   EFA;
+    ETF14.AGG =   AGG;
+    ETF14.IAU =   IAU;
+    ETF14.IYR =   IYR;
+    ETF14.RateMat = dayRateMat;
+    ETF14.RateSet = dayRateSet;
+    
+    save('ROR14_Feb05_Dec10','-v7.3','ETF14');    
 end
 
 function newdataset = computeRate(data) 
     [n, ~] = size(data);
-    data.Prev_Adj_Close = [data.Adj_Close(2:n);NaN];
+    data.Prev_Adj_Close = [NaN; data.Adj_Close(1:n-1)];    
     data.Rate = 100*(data.Adj_Close - data.Prev_Adj_Close)./data.Prev_Adj_Close;
     newdataset = data;
 end
-
-function combineROR(dateVec,dayRateMat)
-    outfile = 'dayROR_ETF14';
-    dayROR_ETF14 = dataset({dateVec,'Date'}, {dayRateMat, 'SPY', 'IJH','IJR','IYY','XLE', 'EWZ','EWJ','EWH','EEM','EZU','EFA','AGG','IAU','IYR'});
-    save(outfile,'-v7.3','dayROR_ETF14');
-end
-
-
-
