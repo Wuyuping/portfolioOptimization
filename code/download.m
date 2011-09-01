3ba800d078263c814e405966b535aa566b2abe14
Thanh Doan <tcdoan@gmail.com>
2011-08-31 21:14:55 -0500

    merge download and download_etf to one

--- code/download.m
+++ code/download.m
@@ -10,30 +10,87 @@
 % 'Volume'
 % 'Adj Close'
 
-y = yahoo('http://download.finance.yahoo.com');
-start_date = 'Jan 31 2005';
-end_date = 'Aug 30 2011';
+function download() 
 
-% SPY IJH IJR	IYY	
-SPY_mat = fetch(y,'SPY',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date );
-IJH_mat = fetch(y,'IJH',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-IJR_mat = fetch(y,'IJR',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-IYY_mat = fetch(y,'IYY',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
+  y = yahoo('http://download.finance.yahoo.com');
+  start_date = 'Jan 31 2005';
+  end_date = 'Aug 30 2011';
+  save_file = 'ETF14_Feb05_Aug11.mat';
 
-% XLE	EWZ	EWJ	EWH	
-XLE_mat = fetch(y,'XLE',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-EWZ_mat = fetch(y,'EWZ',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-EWJ_mat = fetch(y,'EWJ',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-EWH_mat = fetch(y,'EWH',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
+  % SPY IJH IJR	IYY
+  SPY_mat = fetching('SPY');
+  SPY = convert_toDS(SPY_mat);
+  clear SPY_mat;
+    
+  IJH_mat = fetching('IJH');
+  IJH = convert_toDS(IJH_mat);
+  clear IJH_mat;
 
-% EEM	EZU	EFA	AGG	
-EEM_mat = fetch(y,'EEM',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-EZU_mat = fetch(y,'EZU',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-EFA_mat = fetch(y,'EFA',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
-AGG_mat = fetch(y,'AGG',{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'},start_date, end_date);
+  IJR_mat = fetching('IJR');
+  IJR = convert_toDS(IJR_mat);
+  clear IJR_mat;
 
-%IAU IYR
-IAU_mat = fetch(y,'IAU',{'Close', 'High', 'Low', 'Open',  'Volume', 'Adj Close'},start_date, end_date);
-IYR_mat = fetch(y,'IYR',{'Close', 'High', 'Low', 'Open',  'Volume', 'Adj Close'},start_date, end_date);
+  IYY_mat = fetching('IYY');
+  IYY = convert_toDS(IYY_mat);
+  clear IYY_mat;
 
-save('ETF_14_Feb05_Aug11.mat');
+  % XLE	EWZ	EWJ	EWH	
+  XLE_mat = fetching('XLE');
+  XLE = convert_toDS(XLE_mat);
+  clear XLE_mat; 
+
+  EWZ_mat = fetching('EWZ');
+  EWZ  = convert_toDS(EWZ_mat);
+  clear EWZ_mat;
+
+  EWJ_mat = fetching('EWJ');
+  EWJ = convert_toDS(EWJ_mat);
+  clear EWJ_mat;
+
+  EWH_mat = fetching('EWH');
+  EWH = convert_toDS(EWH_mat);
+  clear EWH_mat;
+
+  % EEM	EZU	EFA	AGG	
+  EEM_mat = fetching('EEM');
+  EEM = convert_toDS(EEM_mat);
+  clear EEM_mat;
+
+  EZU_mat = fetching('EZU');
+  EZU = convert_toDS(EZU_mat);
+  clear EZU_mat;
+
+  EFA_mat = fetching('EFA');
+  EFA = convert_toDS(EFA_mat);
+  clear EFA_mat;
+
+  AGG_mat = fetching('AGG');
+  AGG = convert_toDS(AGG_mat);
+  clear AGG_mat;
+
+  % IAU IYR
+  IAU_mat = fetching('IAU');
+  IAU = convert_toDS(IAU_mat);
+  clear IAU_mat;
+
+  IYR_mat = fetching('IYR');
+  IYR = convert_toDS(IYR_mat);
+  clear IYR_mat;    
+
+  save(save_file);
+end
+
+function datamat = fetching(etf_symbol) 
+  global y;
+  global start_date;
+  global end_date;
+  
+  datamat = fetch(y, etf_symbol,{'Close', 'High', 'Low', 'Open', 'Volume', 'Adj Close'}, start_date, end_date);
+end
+
+function myds = convert_toDS(data_mat)
+  nrows = size(data_mat, 1);
+  reverse_idx = nrows : -1 : 1;
+  new_mat = data_mat(reverse_idx, :);
+  myds = dataset({cellstr(datestr(new_mat(:,1),'mm/dd/yyyy')),'Date'},{new_mat(:,2:7), 'Close', 'High', 'Low', 'Open', 'Volume', 'Adj_Close'});
+end
