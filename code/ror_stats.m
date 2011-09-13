2f45ab366dce75b0062719cf8ce694653accbaab
Thanh Doan <tcdoan@gmail.com>
2011-09-12 22:46:50 -0500

    compute ror stats and store in a dataset

--- code/ror_stats.m
+++ code/ror_stats.m
@@ -2,30 +2,29 @@
 % for low, medium, high volatilbility periods 
 
 function ror_stats()
-  load Variables;
   load Partition_Feb05_Sep11;
 
   while (true) 
-      disp('1) compute stats for low volatility period');
-      disp('2) compute stats for medium volatility period');
-      disp('3) compute stats for high volatility period');
-      disp('4) compute stats for all periods ');      
+      disp('1) compute stats for 3 volatility periods');
       disp('9) box plots of return rate for 3 volatility periods');
       disp('-1) for exit');
 
       option_id = input('Enter option id: '); 
-      if option_id == -1
+      
+      switch option_id
+        case -1
           break;
-      end
-
-      if option_id == 9
+        case 1
+          compute_stats(ETF14.Rate);
+        case 9
           box_plots(ETF14.Rate);
+        otherwise
+          continue;
       end
-
   end
 end
 
-function box_plots(data) 
+function box_plots(data)
   for i = 2:15    
     h = figure; 
     boxplot(double(data(:,i)), data.volatile);
@@ -35,3 +34,29 @@ function box_plots(data)
     saveas(h,data.Properties.VarNames{i},'jpg');
   end  
 end
+
+function compute_stats(data)
+  load Variables;
+  
+  stats.data =  data;
+  
+  data_vars = Variables(2:15,1);
+  stats.datavars = data_vars;
+  
+  stat_vars = {@mean, 'sem', @std, @min, @max, @range, @median, 'meanci', 'predci'};
+  stats.statvars = stat_vars;
+  
+  group = {'volatile'};
+  stats.group = group;
+  
+  volatile_stat = grpstats(data,group,stat_vars, 'DataVars',data_vars);
+  volatile_stat(:,{'volatile'})=[];  
+  all_stat = grpstats(data,[],stat_vars, 'DataVars',data_vars);    
+  stats.stat = [volatile_stat; all_stat];
+  
+  low_corr = 
+  save('ror_feb05_sep11_stats','-v7.3','stats');
+  correctairspeed
+end
+       
+
